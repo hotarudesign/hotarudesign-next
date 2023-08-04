d1be5e603519b2a56185605e0cd988a3d90f8d23
hotarudesign <hotarudesign1220@gmail.com>
2023-08-04 16:34:35 +0900

    ♻️ Navのリファクタリング

--- src/component/Header/Nav/index.jsx
+++ src/component/Header/Nav/index.jsx
@@ -1,17 +1,21 @@
 import Link from "next/link";
 import classes from "src/component/Header/Nav/Nav.module.scss";
 
-const navItem = ["About", "Works", "Contact"];
+const NAV_ITEMS = [
+  { href: "/about", label: "About" },
+  { href: "/works", label: "Works" },
+  { href: "/contact", label: "Contact" },
+];
 
 export const Nav = () => {
   return (
     <nav className={classes.gnav}>
       <ul className={classes.gnav__list}>
-        {navItem.map((item) => {
+        {NAV_ITEMS.map((item) => {
           return (
-            <li key={item} className={classes.gnav__item}>
-              <Link href="" legacyBehavior>
-                <a>{item}</a>
+            <li key={item.href} className={classes.gnav__item}>
+              <Link href={item.href} legacyBehavior>
+                <a>{item.label}</a>
               </Link>
             </li>
           );
