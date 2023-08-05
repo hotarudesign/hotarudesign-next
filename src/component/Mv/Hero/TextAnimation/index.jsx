0601e24e6dc481abd9f221ae50edc71c1dcff284
hotarudesign <hotarudesign1220@gmail.com>
2023-08-05 14:05:18 +0900

    ✨ TextAnimationの追加

---
+++ src/component/Mv/Hero/TextAnimation/index.jsx
@@ -0,0 +1,39 @@
+import { useCallback } from "react";
+import { gsap } from "gsap";
+import { TextPlugin } from "gsap/dist/TextPlugin";
+import classes from "src/component/Mv/Hero/TextAnimation/TextAnimation.module.scss";
+
+const TextAnimation = (props) => {
+  const textRef = useCallback((node) => {
+    if (node !== null) {
+      const text = node.innerHTML; //テキストを読み込む
+      const height = node.clientHeight; //高さを取得する
+      node.innerHTML = ""; //テキストを削除する
+      node.style.height = height + "px"; //高さを設定する
+      setAnimation(text);
+    }
+  }, []);
+
+  const setAnimation = (text) => {
+    const numText = text.length;
+    const selector = "#" + props.section;
+
+    gsap.registerPlugin(TextPlugin);
+    gsap.to(`${selector} .animation-text`, {
+      duration: numText * 0.1,
+      delay: 0.8,
+      ease: "none",
+      text: {
+        value: text,
+      },
+    });
+  };
+
+  return (
+    <p ref={textRef} className={`${classes.main} animation-text`}>
+      {props.children}
+    </p>
+  );
+};
+
+export default TextAnimation;
